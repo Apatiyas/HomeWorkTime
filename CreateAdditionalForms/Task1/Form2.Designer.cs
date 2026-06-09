0b5da74239bef5b5500f390daf537ff221b3dc58
Apatiyas <sarmatkaloev921@gmail.com>
2026-06-10 02:20:18 +0300

    Update Form2.Designer.cs

--- CreateAdditionalForms/Task1/Form2.Designer.cs
+++ CreateAdditionalForms/Task1/Form2.Designer.cs
@@ -29,36 +29,36 @@
             this.labelResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
 
-            // textBoxFolder
+            
             this.textBoxFolder.Location = new System.Drawing.Point(12, 12);
             this.textBoxFolder.Size = new System.Drawing.Size(300, 20);
 
-            // buttonBrowse
+            
             this.buttonBrowse.Location = new System.Drawing.Point(320, 10);
             this.buttonBrowse.Size = new System.Drawing.Size(80, 23);
             this.buttonBrowse.Text = "Обзор";
             this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
 
-            // textBoxMask
+            
             this.textBoxMask.Location = new System.Drawing.Point(12, 45);
             this.textBoxMask.Size = new System.Drawing.Size(100, 20);
             this.textBoxMask.Text = "*.doc";
 
-            // buttonSearch
+            
             this.buttonSearch.Location = new System.Drawing.Point(120, 43);
             this.buttonSearch.Size = new System.Drawing.Size(80, 23);
             this.buttonSearch.Text = "Искать";
             this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
 
-            // listBoxFiles
+            
             this.listBoxFiles.Location = new System.Drawing.Point(12, 75);
             this.listBoxFiles.Size = new System.Drawing.Size(388, 200);
 
-            // labelResult
+         
             this.labelResult.AutoSize = true;
             this.labelResult.Location = new System.Drawing.Point(210, 48);
 
-            // Form2
+            
             this.ClientSize = new System.Drawing.Size(412, 290);
             this.Controls.Add(this.textBoxFolder);
             this.Controls.Add(this.buttonBrowse);
