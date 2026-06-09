a4ff13ee9e80e3a59617e9c8f1f0102ed8fcf70c
Apatiyas <sarmatkaloev921@gmail.com>
2026-06-10 02:21:06 +0300

    Update Form1.Designer.cs

--- CreateAdditionalForms/Task1/Form1.Designer.cs
+++ CreateAdditionalForms/Task1/Form1.Designer.cs
@@ -30,9 +30,7 @@
         {
             button1 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
+           
             button1.Location = new Point(334, 176);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
@@ -40,9 +38,7 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
-            // 
-            // Form1
-            // 
+           
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
